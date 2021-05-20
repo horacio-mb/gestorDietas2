532a5314778855b58233f64e318735ffcccd9d2b
Horacio Molina <horaciomolinab0@gmail.com>
2021-05-19 23:53:37 -0400

    Dieta Comida cls

--- gestorDietas/capaNegocio/DietaComida.cs
+++ gestorDietas/capaNegocio/DietaComida.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using capaDatos;
 
 namespace capaNegocio
 {
-    class DietaComida
+    class DietaComida : clsConexion
     {
+        private int idDieta;
+        private int idComida;
+        private string distribucion;
+
+        public DietaComida()
+        {
+            idDieta = 0;
+            idComida = 0;
+            distribucion = "";
+        }
+        public int IdDieta
+        {
+            get { return this.idDieta; }
+            set { this.idDieta = value; }
+        }
+        public int IdComida
+        {
+            get { return this.idComida; }
+            set { this.idComida = value; }
+        }
+
+        public string Distribucion
+        {
+            get { return this.distribucion; }
+            set { this.distribucion = value; }
+        }
+        public bool guardar()
+        {
+            iniciarSP("guardarDetalleVenta");
+            parametroInt(idComida, "id_co");
+            parametroVarchar(distribucion, "dis", 30);
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
+        public bool eliminar()
+        {
+            iniciarSP("eliminarDietaComida");
+            parametroInt(idDieta, "id_d");
+            if (ejecutarSP() == true) { return true; } else { return false; }
+        }
     }
 }
